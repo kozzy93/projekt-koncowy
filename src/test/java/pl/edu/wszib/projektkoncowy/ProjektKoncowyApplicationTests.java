caed774f077f7a496cf4d1ec38f6636b2c5c1b60
kozzy93 <krzysztof_ozga@poczta.fm>
2022-06-11 13:59:55 +0200

    Initial commit

---
+++ src/test/java/pl/edu/wszib/projektkoncowy/ProjektKoncowyApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edu.wszib.projektkoncowy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjektKoncowyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
